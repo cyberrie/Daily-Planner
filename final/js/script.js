ee91c5f47f0165f2b0937fdc6a6ed813ecacbdc2
helena <helena.gilja@gmail.com>
2023-01-20 11:09:21 +0000

    id of text area changed to 24hr format but numbers only

--- final/js/script.js
+++ final/js/script.js
@@ -8,3 +8,16 @@ setInterval(function () {
 }, 1000);
 
 // Timeblock Grid
+
+// current time
+
+// currentTime to determine if the hour matches for color
+
+// if past color gray
+// if future red color
+// if more than 1hr left (future) color green
+// allow user to enter and event when they click timeblock
+
+$("textarea").on("click", function () {});
+//save the event in local storage when save button is clicked in that timeblock
+// persist events between refreshes of a page
